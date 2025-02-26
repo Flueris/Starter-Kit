4fe003c746ee36118c7d174f6b15553403ea2cf5
Flueris <support@flueris.com>
2025-02-26 18:02:40 +0530

    Update 26.02.2025

--- FluerisStarterKit/Program.cs
+++ FluerisStarterKit/Program.cs
@@ -2,10 +2,14 @@ using FluerisStarterKit;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using Flueris.Components;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<MessageService>();
 
 await builder.Build().RunAsync();
